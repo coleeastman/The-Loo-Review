e814b039f63f99bfe4dc5a82bfacd5b86e801b4d
coleeastman <cole.eastman@tcognition.com>
2025-01-22 14:00:34 -0700

    Changed on success message

--- frontend/src/components/ReviewForm.js
+++ frontend/src/components/ReviewForm.js
@@ -33,19 +33,12 @@ const ReviewForm = ({ restroom }) => {
             setError(json.error);
             setMessage(null);
         }
-        // if (response.ok) {
-        //     setMessage({ text: json.message, type: 'success' });
-        //     setEmptyFields([]);
-        //     setRestroomName('');
-        //     setStars('');
-        //     setReview('');
-        //     setError(null);
-        //     console.log('new review added');
-        // }
+
         if (response.ok) {
-            console.log(json.message); // Debug the response
-            setMessage({ text: json.message, type: 'success' });
-            setError(null); // Clear any previous errors
+            console.log(json.message);
+            setMessage({ text: "Review Submitted", type: 'success' });
+            //     setEmptyFields([]);
+            setError(null);
             setRestroomName('');
             setStars('');
             setReview('');
